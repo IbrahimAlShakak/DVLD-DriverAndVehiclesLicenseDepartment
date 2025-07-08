75a5b65f21e8c11e52f702f17a531be52c33a34e
IbrahimAlShakak <Ibrahim.al_shakak@yahoo.com>
2025-07-08 12:17:29 +0200

    Added a new Form to use the feature of finding a Person Info by the National Number ot by ID.

---
+++ People/frmFindPerson.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace DVLD_DriverAndVehiclesLicenseDepartment.People
+{
+    partial class frmFindPerson
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctrlPersonInfoCardWithFilter1 = new DVLD_DriverAndVehiclesLicenseDepartment.People.Controls.ctrlPersonInfoCardWithFilter();
+            this.lblTitel = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ctrlPersonInfoCardWithFilter1
+            // 
+            this.ctrlPersonInfoCardWithFilter1.FilterEnabled = true;
+            this.ctrlPersonInfoCardWithFilter1.Location = new System.Drawing.Point(12, 52);
+            this.ctrlPersonInfoCardWithFilter1.Name = "ctrlPersonInfoCardWithFilter1";
+            this.ctrlPersonInfoCardWithFilter1.ShowAddNewPerson = true;
+            this.ctrlPersonInfoCardWithFilter1.Size = new System.Drawing.Size(908, 474);
+            this.ctrlPersonInfoCardWithFilter1.TabIndex = 0;
+            // 
+            // lblTitel
+            // 
+            this.lblTitel.AutoSize = true;
+            this.lblTitel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitel.ForeColor = System.Drawing.Color.Red;
+            this.lblTitel.Location = new System.Drawing.Point(328, 13);
+            this.lblTitel.Name = "lblTitel";
+            this.lblTitel.Size = new System.Drawing.Size(177, 36);
+            this.lblTitel.TabIndex = 1;
+            this.lblTitel.Text = "Find Person";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackgroundImage = global::DVLD_DriverAndVehiclesLicenseDepartment.Properties.Resources.CloseIcon;
+            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnClose.Location = new System.Drawing.Point(361, 532);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(129, 34);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmFindPerson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 587);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblTitel);
+            this.Controls.Add(this.ctrlPersonInfoCardWithFilter1);
+            this.Name = "frmFindPerson";
+            this.ShowIcon = false;
+            this.Text = "Find Person";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Controls.ctrlPersonInfoCardWithFilter ctrlPersonInfoCardWithFilter1;
+        private System.Windows.Forms.Label lblTitel;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
--- People/frmPersonInfocs.Designer.cs
+++ People/frmPersonInfocs.Designer.cs
@@ -30,40 +30,55 @@
         {
             this.ctrlPersonCard1 = new DVLD_DriverAndVehiclesLicenseDepartment.People.Controls.ctrlPersonInfoCard();
             this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ctrlPersonCard1
             // 
-            this.ctrlPersonCard1.Location = new System.Drawing.Point(12, 12);
+            this.ctrlPersonCard1.Location = new System.Drawing.Point(12, 48);
             this.ctrlPersonCard1.Name = "ctrlPersonCard1";
-            this.ctrlPersonCard1.Size = new System.Drawing.Size(1073, 425);
+            this.ctrlPersonCard1.Size = new System.Drawing.Size(906, 408);
             this.ctrlPersonCard1.TabIndex = 0;
             // 
             // btnClose
             // 
-            this.btnClose.Image = global::DVLD_DriverAndVehiclesLicenseDepartment.Properties.Resources.CloseIcon;
+            this.btnClose.BackgroundImage = global::DVLD_DriverAndVehiclesLicenseDepartment.Properties.Resources.CloseIcon;
+            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnClose.Location = new System.Drawing.Point(521, 442);
+            this.btnClose.Location = new System.Drawing.Point(415, 461);
             this.btnClose.Margin = new System.Windows.Forms.Padding(2);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(102, 37);
+            this.btnClose.Size = new System.Drawing.Size(100, 36);
             this.btnClose.TabIndex = 7;
-            this.btnClose.Text = "Close";
             this.btnClose.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(386, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 36);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Person Info";
             // 
             // frmPersonInfocs
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(930, 490);
+            this.ClientSize = new System.Drawing.Size(924, 514);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.ctrlPersonCard1);
             this.Name = "frmPersonInfocs";
             this.ShowIcon = false;
             this.Text = "frmPersonInfocs";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,5 +86,6 @@
 
         private Controls.ctrlPersonInfoCard ctrlPersonCard1;
         private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
     }
 }
